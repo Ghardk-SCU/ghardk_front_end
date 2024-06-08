b7bed75a67d69cc9537d2a94dc7fcf5e45630ffe
Moemen Adam <mmuummnna@gmail.com>
2024-06-08 12:48:13 +0300

    fix navbar bug (when opened)

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,14 +1,14 @@
 import Welcome from './Welcome/Welcome'
-import Categories from './Categories/Categories'
-import PopularProd from './PopularProd/PopularProd'
+import Categories from './Categories/index'
+import PopularProd from './PopularProd/index'
+import ForYou from './ForYou/index'
 export default function Home() {
   return (
     <>
       <Welcome />
       <Categories />
       <PopularProd />
-      {/* <Categories /> */}
-
+      <ForYou />
     </>
   )
 }
