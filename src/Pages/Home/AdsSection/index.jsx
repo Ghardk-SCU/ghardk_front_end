2c91a0e26a9a7742035f3e305bd9705c5abe74dc
Moemen Adam <mmuummnna@gmail.com>
2024-06-15 04:26:54 +0300

    finish Top Rated Page

--- src/Pages/Home/AdsSection/index.jsx
+++ src/Pages/Home/AdsSection/index.jsx
@@ -4,6 +4,8 @@ import Ad from './assets/Ad.jpg'
 import InView from '../../../utils/InView'
 import { HiArrowLongRight } from "react-icons/hi2";
 
+import { clipPathLeftToRight } from '../../../Store/AnimationValues'
+
 const Ads = [Ad, Ad, Ad]
 
 export default function AdsSection() {
@@ -11,10 +13,7 @@ export default function AdsSection() {
   const dragMotion = useMotionValue(0)
   const DragBuffer = 10
 
-  const variants = {
-    hidden: { opacity: 0, clipPath: 'inset(0 100% 0 0)' },
-    visible: { opacity: 0.8, clipPath: 'inset(0 0 0 0)', transition: { duration: 1 } },
-  }
+  const variants = clipPathLeftToRight(0)
 
   const handleMove = (type = "drag") => {
     const x = dragMotion.get()
@@ -31,7 +30,7 @@ export default function AdsSection() {
     }
   }
   return (
-    <div className="relative w-full h-screen center">
+    <div className="relative w-full h-screen center overflow-hidden">
       <InView variants={variants} classes="w-screen h-full center">
         <div className='relative w-full h-[60%]'>
           <motion.div
@@ -65,7 +64,7 @@ const Functionality = ({ handleMove, imgTurn, setImgTurn, AdsLength }) => {
   return (
     <>
       <div className='absolute left-10 bottom-1/4'>
-        <button className='text-white self-end bg-Black rounded-full py-4 px-6 sm:py-7 sm:px-12
+        <button className='text-white self-end bg-Black rounded-full py-5 px-6 sm:py-7 sm:px-12
             relative overflow-hidden inline-block z-10 focus:outline-none
             before:absolute before:top-0 before:left-[-100%] before:w-full before:h-full before:-z-10
             before:rounded-inherit before:bg-[#505050] before:bg-opacity-40 
@@ -74,12 +73,12 @@ const Functionality = ({ handleMove, imgTurn, setImgTurn, AdsLength }) => {
           VIEW PROFILE
         </button>
       </div>
-      {imgTurn > 0 && <div className='absolute left-10 top-1/2 -translate-y-1/2'>
+      {imgTurn > 0 && <div className='absolute left-2 sm:left-10 top-1/2 -translate-y-1/2'>
         <button onClick={() => { handleMove("-1") }} className='rotate-180 self-center text-white bg-Black/50 rounded-full p-5 group hover:bg-white/50 duration-300'>
           <HiArrowLongRight className='size-6 sm:size-8 group-hover:text-Black group-hover:translate-x-[10px] duration-300' />
         </button>
       </div>}
-      {imgTurn < AdsLength - 1 && <div className='absolute right-10 top-1/2 -translate-y-1/2'>
+      {imgTurn < AdsLength - 1 && <div className='absolute right-2 sm:right-10 top-1/2 -translate-y-1/2'>
         <button onClick={() => { handleMove("+1") }} className='self-center text-white bg-Black/50 rounded-full p-5 group hover:bg-white/50 duration-300'>
           <HiArrowLongRight className='size-6 sm:size-8 group-hover:text-Black group-hover:translate-x-[10px] duration-300' />
         </button>
