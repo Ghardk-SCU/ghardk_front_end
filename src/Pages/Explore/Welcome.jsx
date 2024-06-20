a30e60dc6212b5d4f4923d04a32327e1129363d5
Moemen Adam <mmuummnna@gmail.com>
2024-06-20 20:57:14 +0300

    fix explore img (responsive in bigscreens and hidden when bellow  1200px)

--- src/Pages/Explore/Welcome.jsx
+++ src/Pages/Explore/Welcome.jsx
@@ -5,14 +5,14 @@ import Bear from '../../assets/Bear.png'
 export default function Welcome() {
   return (
     <div className="relative w-full min-h-screen center select-none">
-      <img className='imgSettings absolute -bottom-[40%] -left-[30%] scale-[1] hidden lg1:inline-block' src={Left} alt="" />
+      <img className='h-[210vh] imgSettings absolute -bottom-[40%] -left-[33%] scale-[1] hidden lg2:inline-block' src={Left} alt="" />
+      <img className='h-[210vh] imgSettings absolute -top-[40%] -right-[38%] -rotate-6 hidden lg2:inline-block' src={Right} alt="" />
       <div className='relative w-min h-min'>
         <h1 className='EBGaramond uppercase font-medium text-[25vw] lg:text-[17vw] select-none'>
           Shop
         </h1>
         <img src={Bear} alt="loadingBear" className='absolute top-[44%] right-[28%] w-2/12 select-none' />
       </div>
-      <img className='imgSettings absolute -top-[40%] -right-[33%] -rotate-6 hidden lg1:inline-block' src={Right} alt="" />
     </div>
   )
 }
